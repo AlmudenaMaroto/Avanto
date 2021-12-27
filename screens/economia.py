fa1d4af8e83115f11fb8263d9046d047848c9cb6
AlmudenaMaroto <73901561+AlmudenaMaroto@users.noreply.github.com>
2021-12-27 15:18:23 +0100

    graph

--- screens/economia.py
+++ screens/economia.py
@@ -5,7 +5,7 @@ import os
 from datetime import datetime
 import time
 from calendar import timegm
-
+import charts_almu
 
 class Economia(MDScreen):
     def __init__(self, **kwargs):
@@ -23,6 +23,7 @@ class Economia(MDScreen):
         epochs = []
         importes = []
         saldos = []
+        saldos_str = []
         saldo = 0
 
         con = sqlite3.connect(self.path_app + '/movimientos.db')
@@ -30,49 +31,39 @@ class Economia(MDScreen):
         orden_execute = 'select * from movimientos ORDER BY ID ASC'
         cursor.execute(orden_execute)
         for i in cursor:
-            saldo = saldo + i[4]
-            if i[0] % 5 == 0:
+            saldo = round(saldo + i[4], 2)
+            if True:
                 try:
                     utc_time = time.strptime(i[1], "%d/%m/%Y")
                     epoch_time = timegm(utc_time)
-                    fechas.append(i[1])
+                    if i[0] % 500 == 0:
+                        fechas.append(i[1])
+                        saldos_str.append(str(saldo))
+                    else:
+                        fechas.append('')
+                        saldos_str.append('')
                     epochs.append(epoch_time)
                     importes.append(i[4])
                     saldos.append(saldo)
                 except ValueError:
                     pass
         con.close()
-        # Para no petar el grafico, cogemos solo 30 valores.
+        # Para no petar el grafico, cogemos menos valores
+        maximo_saldo = max(saldos)
+        minimo_saldo = min(saldos)
+        max_epoch = max(epochs)
+        min_epoch = min(epochs)
 
         self.ids.chart1.x_values = epochs
         self.ids.chart1.x_labels = fechas
         self.ids.chart1.y_values = saldos
-        # self.ids.chart1.y_labels = saldos
-
+        self.ids.chart1.y_labels = saldos_str
 
     def set_text(self, args):
         self.ids._label.text = f"{args[1]} [{args[2]},{args[3]}]"
 
     def update(self):
-        chart1 = self.ids.chart1
-        chart1.x_values = [2, 8, 12, 35, 40, 43, 56]
-        chart1.y_values = [3, 2, 1, 16, 0, 1, 10]
-        chart1.update()
-
-        chart2 = self.ids.chart2
-        chart2.x_values = [2, 8, 12, 35, 40, 43, 56]
-        chart2.y_values = [3, 2, 1, 15, 0, 1, 10]
-        chart2.update()
-
-        chart3 = self.ids.chart3
-        chart3.x_values = [2, 8, 12, 35, 40, 43, 56]
-        chart3.y_values = [3, 2, 1, 15, 0, 1, 10]
-        chart3.update()
-
-        chart4 = self.ids.chart4
-        chart4.x_labels = ["XYZ", "Second", "Third", "Last"]
-        chart4.y_labels = ["XYZ", "Second", "Third", "Last"]
-        chart4.update()
+        pass
 
     def choose_etapa(self):
         pass
@@ -86,21 +77,19 @@ class Economia(MDScreen):
 
 Builder.load_string(
     """
-<Evolucion_temporal@AKLineChart>
+<Evolucion_temporal@AKLineChart_Almu>
     size_hint_y: None
     height: dp(180)
     x_values: []
     y_values: []
     label_size: dp(12)
     
-<MyAKLineChart@AKLineChart>
+<MyAKLineChart_Almu@AKLineChart_Almu>
     size_hint_y: None
     height: dp(180)
     x_values: [0, 5, 8, 15]
     y_values: [0, 10, 6, 8]
     label_size: dp(12)
-
-
 
 <Economia>
     on_leave: pass
@@ -123,27 +112,26 @@ Builder.load_string(
                     id: chart1
                     labels: False
                     anim: True
-                    #bg_color: 0.6, 0, 0, 1
-                    #circles_color: [0.4, 0.4, 1, 1]
                     circles: False
+                    bg_color: 106/255, 188/255, 206/255, 1
                     labels: False
                     on_select: root.set_text(args)
                     line_width:dp(1)
 
-                MyAKLineChart:
+                MyAKLineChart_Almu:
                     id: chart2
                     labels: False
                     anim: True
                     on_select: root.set_text(args)
 
-                MyAKLineChart:
+                MyAKLineChart_Almu:
                     id: chart3
                     labels: True
                     anim: True
                     lines: False
                     on_select: root.set_text(args)
 
-                MyAKLineChart:
+                MyAKLineChart_Almu:
                     id: chart4
                     labels: True
                     anim: True
